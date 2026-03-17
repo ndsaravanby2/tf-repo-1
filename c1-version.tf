efa1d187391c5a6a031a68777290125ea43de79f
ndsaravanby2 <ndsaravanby2@gmail.com>
2026-03-17 14:55:46 +0530

    second-commit

--- c1-version.tf
+++ c1-version.tf
@@ -1,9 +1,13 @@
 terraform {
     required_version = "~> 1.0"
   required_providers {
-    name = {
+    aws = {
       source = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
---
+++ c2-resource.tf
@@ -0,0 +1,4 @@
+resource "aws_instance" "nds-cloud-vm" {
+ ami = "ami-0532be01f26a3de55"
+ instance_type = "t2.micro" 
+}
